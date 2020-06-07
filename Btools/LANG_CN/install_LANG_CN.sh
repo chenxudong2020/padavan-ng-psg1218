0a119f27e62ab53bba03317a7f16705d4a1fef15
hminid <hminid@163.com>
2020-06-07 15:26:11 +0800

    Add files via upload

---
+++ Btools/LANG_CN/install_LANG_CN.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DESTDIR=/opt/rt-n56u
+ROOTDIR=`pwd`
+
+	echo "--------------开始复制语言文件------------------"
+	if [ -e "${ROOTDIR}/Makefile" ] ; then
+		cp -f "${ROOTDIR}/Makefile" "${DESTDIR}/trunk/user/www/"
+	fi
+  #
+  if [ -e "${ROOTDIR}/CN.dict" ] ; then
+		cp -f "${ROOTDIR}/CN.dict" "${DESTDIR}/trunk/user/www/dict/"
+	fi
+	echo "--------------复制语言文件结束------------------"
