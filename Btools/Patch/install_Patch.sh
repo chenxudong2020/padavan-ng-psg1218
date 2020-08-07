c1c67e2b6ccad61b8f4432ea76a44511722e42a6
chenzc@prolog-int.com <chenzc@prolog-int.com>
2020-08-07 20:14:34 +0800

    Update install_Patch.sh

--- Btools/Patch/install_Patch.sh
+++ Btools/Patch/install_Patch.sh
@@ -6,8 +6,8 @@ ROOTDIR=`pwd`
 
 
 echo "--------------开始应用PSG1218 EEPROM补丁----------------------"
- \cp -f ${ROOTDIR}/Stock_v21.4.6.10/MT7612E_EEPROM.bin ${DESTDIR}/trunk/vendors/Ralink/MT7620/MT7612E_EEPROM.bin 
- \cp -f ${ROOTDIR}/Stock_v21.4.6.10/MT7620_AP_2T2R-4L_V15.BIN /opt/rt-n56u/trunk/vendors/Ralink/MT7620/MT7620_AP_2T2R-4L_external_LNA_external_PA_V15.bin
+ \cp -f ${ROOTDIR}/Stock_v21.4.6.10/MT7612E_EEPROM.bin ${DESTDIR}/trunk/vendors/Mediatek/MT7620/MT7612E_EEPROM.bin 
+ \cp -f ${ROOTDIR}/Stock_v21.4.6.10/MT7620_AP_2T2R-4L_V15.BIN /opt/rt-n56u/trunk/vendors/Mediatek/MT7620/MT7620_AP_2T2R-4L_external_LNA_external_PA_V15.bin
 echo "--------------应用PSG1218 EEPROM补丁结束------------------"
     
  
