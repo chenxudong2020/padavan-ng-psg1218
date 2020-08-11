3a693f3f46982adb99fdd32a584f78dd139626d4
chenzc@prolog-int.com <chenzc@prolog-int.com>
2020-08-11 09:02:42 +0800

    Revert "fix ssid"
    This reverts commit 500be9aa690d82d15327f54c7504f536bb6e0a47.

--- Btools/LANG_CN/install_LANG_CN.sh
+++ Btools/LANG_CN/install_LANG_CN.sh
@@ -18,7 +18,6 @@ ROOTDIR=`pwd`
  # trunk/user/www/n56u_ribbon_fixed/general.js
 \cp -f ${ROOTDIR}/SSID/aspbw.c ${DESTDIR}/trunk/user/httpd/aspbw.c
 \cp -f ${ROOTDIR}/SSID/httpd.h ${DESTDIR}/trunk/user/httpd/httpd.h
-\cp -f ${ROOTDIR}/SSID/common_ex.c ${DESTDIR}/trunk/user/rc/common_ex.c
 \cp -f ${ROOTDIR}/SSID/general.js ${DESTDIR}/trunk/user/www/n56u_ribbon_fixed/general.js
     echo "--------------复制中文SSID支持文件结束----------"
 	
