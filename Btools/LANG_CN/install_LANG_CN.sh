7fd9c1dd1cb439c1aeadecefe2f26ed65a712813
chenzc@prolog-int.com <chenzc@prolog-int.com>
2020-08-11 09:02:46 +0800

    Revert "添加中文ssid支持"
    This reverts commit 5032db79c262874bcf72dd550ead137fa3582371.

--- Btools/LANG_CN/install_LANG_CN.sh
+++ Btools/LANG_CN/install_LANG_CN.sh
@@ -12,12 +12,3 @@ ROOTDIR=`pwd`
 		cp -f "${ROOTDIR}/CN.dict" "${DESTDIR}/trunk/user/www/dict/"
 	fi
 	echo "--------------复制语言文件结束------------------"
-    echo "--------------开始复制中文SSID支持文件----------"
- # trunk/user/httpd/aspbw.c
- # trunk/user/httpd/httpd.h
- # trunk/user/www/n56u_ribbon_fixed/general.js
-\cp -f ${ROOTDIR}/SSID/aspbw.c ${DESTDIR}/trunk/user/httpd/aspbw.c
-\cp -f ${ROOTDIR}/SSID/httpd.h ${DESTDIR}/trunk/user/httpd/httpd.h
-\cp -f ${ROOTDIR}/SSID/general.js ${DESTDIR}/trunk/user/www/n56u_ribbon_fixed/general.js
-    echo "--------------复制中文SSID支持文件结束----------"
-	
