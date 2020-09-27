efc3d077065228e83ed630f59e8b0b02d58b35ee
chengxudong2020 <872603935@qq.com>
2020-09-28 00:01:36 +0800

    fix chipId

--- Atools/install_Patch.sh
+++ Atools/install_Patch.sh
@@ -9,6 +9,8 @@ echo "--------------开始应用补丁----------------------"
   \cp -f ${ROOTDIR}/build.sh ${DESTDIR}/build.sh
   \cp -f ${ROOTDIR}/diy/init.c ${DESTDIR}/release/src/router/rc/init.c
   \cp -f ${ROOTDIR}/diy/target.mak ${DESTDIR}/release/src-rt/target.mak
+  \cp -f ${ROOTDIR}/diy/ralink_spi.c ${DESTDIR}/release/src-ra-mt7620/linux/linux-2.6.36.x/drivers/mtd/ralink/ralink_spi.c
+  \cp -f ${ROOTDIR}/diy/ralink_bbu_spi.c ${DESTDIR}/release/src-ra-mt7620/linux/linux-2.6.36.x/drivers/mtd/ralink/ralink_bbu_spi.c
 echo "--------------应用补丁结束------------------"
     
  
