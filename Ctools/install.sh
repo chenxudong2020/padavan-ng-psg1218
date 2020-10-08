53d36be1dfe24c63d144feaf97586d3b5e2733c7
chenxudong2020 <872603935@qq.com>
2020-10-08 21:47:41 +0800

    fix

--- Ctools/install.sh
+++ Ctools/install.sh
@@ -5,10 +5,10 @@ ROOTDIR=`pwd`
 
 
 
-echo "--------------开始应用PSG1218 EEPROM补丁----------------------"
- \cp -f ${ROOTDIR}/B70 ${DESTDIR}/trunk/config/boards/HIWIFI
- \cp -f ${ROOTDIR}/pt_hiwifi_128m.config ${DESTDIR}/trunk/config/boards/HIWIFI/
- \cp -f ${ROOTDIR}/B70.config ${DESTDIR}/trunk/configs/templates/hiwifi
-echo "--------------应用PSG1218 EEPROM补丁结束------------------"
+echo "--------------开始应用B70补丁----------------------"
+ cp -f ${ROOTDIR}/B70 ${DESTDIR}/trunk/config/boards/HIWIFI
+ cp -f ${ROOTDIR}/pt_hiwifi_128m.config ${DESTDIR}/trunk/config/boards/HIWIFI/
+ cp -f ${ROOTDIR}/B70.config ${DESTDIR}/trunk/configs/templates/hiwifi
+echo "--------------应用B70补丁结束------------------"
     
  
