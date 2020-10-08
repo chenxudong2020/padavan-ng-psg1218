8797da99f22ca250dcf1a872b720c767cb1609a4
chenxudong2020 <872603935@qq.com>
2020-10-08 21:55:47 +0800

    fix

--- Ctools/install.sh
+++ Ctools/install.sh
@@ -6,8 +6,8 @@ ROOTDIR=`pwd`
 
 
 echo "--------------开始应用B70补丁----------------------"
- cp -f ${ROOTDIR}/B70 ${DESTDIR}/trunk/config/boards/HIWIFI
- cp -f ${ROOTDIR}/pt_hiwifi_128m.config ${DESTDIR}/trunk/config/boards/HIWIFI/
+ cp -f ${ROOTDIR}/B70 ${DESTDIR}/trunk/configs/boards/HIWIFI
+ cp -f ${ROOTDIR}/pt_hiwifi_128m.config ${DESTDIR}/trunk/configs/boards/HIWIFI/
  cp -f ${ROOTDIR}/B70.config ${DESTDIR}/trunk/configs/templates/hiwifi
 echo "--------------应用B70补丁结束------------------"
     
