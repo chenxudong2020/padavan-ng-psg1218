535a210a8e07d67a85ed56b7d430eaebc18fe9bf
chenxudong2020 <872603935@qq.com>
2021-04-24 22:59:48 +0800

    Update defaults.h

--- Btools/LANG_CN/defaults.h
+++ Btools/LANG_CN/defaults.h
@@ -18,7 +18,7 @@
 #ifndef _defaults_h_
 #define _defaults_h_
 
-#define SYS_SHELL		    "/bin/sh"
+#define SYS_SHELL		"/bin/sh"
 #define SYS_EXEC_PATH		"/usr/sbin:/usr/bin:/sbin:/bin"
 #define SYS_EXEC_PATH_OPT	"/opt/sbin:/opt/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 #define SYS_HOME_PATH_ROOT	"/home/admin"
@@ -29,7 +29,7 @@
 #define SYS_GROUP_NOGROUP	"nogroup"
 
 #define DEF_LAN_ADDR		"192.168.1.1"
-#define DEF_LAN_DHCP_BEG	"192.168.1.10"
+#define DEF_LAN_DHCP_BEG	"192.168.1.2"
 #define DEF_LAN_DHCP_END	"192.168.1.244"
 #define DEF_LAN_MASK		"255.255.255.0"
 
@@ -52,5 +52,7 @@
 #else
 #define DEF_HTTPS_CIPH_LIST	"DH+AESGCM:DH+AES256:DH+AES:DH+3DES:RSA+AES:RSA+3DES:!ADH:!MD5:!DSS"
 #endif
+#define DEF_OVPNS_CIPH_LIST	"CHACHA20-POLY1305:AES-256-GCM:AES-128-GCM"
+#define DEF_OVPNC_CIPH_LIST	"CHACHA20-POLY1305:AES-256-GCM:AES-128-GCM"
 
 #endif
