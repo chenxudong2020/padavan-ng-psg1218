d09f65ab2adb1e2ee44929123db536b91008e6f3
chenzc <chenzc@prolog-int.com>
2021-07-17 10:24:35 +0800

    fix

--- Btools/Patch/install_Patch.sh
+++ Btools/Patch/install_Patch.sh
@@ -5,9 +5,9 @@ ROOTDIR=`pwd`
 
 
 
-# echo "--------------开始应用PSG1218 EEPROM补丁----------------------"
-# \cp -f ${ROOTDIR}/Stock_v21.4.6.10/MT7612E_EEPROM.bin ${DESTDIR}/trunk/vendors/Mediatek/MT7612E_EEPROM.bin 
-# \cp -f ${ROOTDIR}/Stock_v21.4.6.10/MT7620_AP_2T2R-4L_V15.BIN /opt/rt-n56u/trunk/vendors/Mediatek/MT7620_AP_2T2R-4L_external_LNA_external_PA_V15.bin
-# echo "--------------应用PSG1218 EEPROM补丁结束------------------"
+ echo "--------------开始应用PSG1218 EEPROM补丁----------------------"
+ \cp -f ${ROOTDIR}/Stock_v21.4.6.10/MT7612E_EEPROM.bin ${DESTDIR}/trunk/vendors/Mediatek/MT7612E_EEPROM.bin 
+ \cp -f ${ROOTDIR}/Stock_v21.4.6.10/MT7620_AP_2T2R-4L_V15.BIN /opt/rt-n56u/trunk/vendors/Mediatek/MT7620_AP_2T2R-4L_external_LNA_external_PA_V15.bin
+ echo "--------------应用PSG1218 EEPROM补丁结束------------------"
     
  
